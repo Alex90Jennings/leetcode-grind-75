a3cead0247fd96b85932db926a7e7addd89e8ad2
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-11-07 17:36:57 +0100

    second solution

--- two-sum.ts
+++ two-sum.ts
@@ -1,11 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
     for(let i = 0; i < nums.length; i++){
-        for(let j = 0; j < nums.length; j++){
-            if(i === j) continue
-            if(nums[i] + nums[j] === target) return [i,j]
-        }
+        const targetSecondNumber: number = target - nums[i]
+        if(nums.indexOf(targetSecondNumber) !== -1) return [i, nums.indexOf(targetSecondNumber)]
     }
     return [0, 0]
 };
 
 console.log(twoSum([2,7,9,3], 12))
+console.log(twoSum([2,7,9,3,11,23,1251,5122,512,12,2131,123], 2256))
