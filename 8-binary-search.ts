6c73e2755a3f56bfca940592cc75e9e205644ad8
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-11-14 11:52:39 +0100

    binary search algo

--- 8-binary-search.ts
+++ 8-binary-search.ts
@@ -1,18 +1,17 @@
 function search(nums: number[], target: number): number {
-    let n:number = nums.length - 1
-    const isTargetCloserToBottom = nums[0] + target < nums[n] - target
-
-    if(isTargetCloserToBottom){
-        for(let i = 0; i <= n; i++) {
-            if(target < nums[i]) return -1
-            if(target === nums[i]) return i
-            continue
-        }
-    } else {
-        for(let i = n; i >= 0; i--) {
-            if(target > nums[i]) return -1
-            if(target === nums[i]) return i
-            continue
-        }
+    let leftPointer: number = 0;
+    let rightPointer: number = nums.length - 1;
+  
+    while (leftPointer <= rightPointer) {
+      const middle: number = leftPointer + Math.floor((rightPointer - leftPointer) / 2);
+      
+      if (nums[middle] < target) {
+        leftPointer = middle + 1;
+      } else if (nums[middle] > target) {
+        rightPointer = middle - 1;
+      } else {
+        return middle;
+      }
     }
-};
+    return -1;
+  };
