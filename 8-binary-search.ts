d952418ef6ff0665810502ab353a8c0815e6fbe1
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-11-14 10:40:40 +0100

    solution 8

--- 8-binary-search.ts
+++ 8-binary-search.ts
@@ -1,3 +1,18 @@
 function search(nums: number[], target: number): number {
+    let n:number = nums.length - 1
+    const isTargetCloserToBottom = nums[0] + target < nums[n] - target
 
+    if(isTargetCloserToBottom){
+        for(let i = 0; i <= n; i++) {
+            if(target < nums[i]) return -1
+            if(target === nums[i]) return i
+            continue
+        }
+    } else {
+        for(let i = n; i >= 0; i--) {
+            if(target > nums[i]) return -1
+            if(target === nums[i]) return i
+            continue
+        }
+    }
 };
