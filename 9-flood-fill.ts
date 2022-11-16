087c99108f7fe3fb46f78c2b2c6100eed4c5607b
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-11-16 18:21:20 +0100

    flood fill

---
+++ 9-flood-fill.ts
@@ -0,0 +1,22 @@
+function floodFill(image: number[][], sr: number, sc: number, colour: number): number[][] {
+    const visitedPixels: number[] = []
+    let currentPixelColour = image[sr][sc];
+    if(currentPixelColour !== colour) {
+        paintPixel(image, sr, sc, currentPixelColour, colour, visitedPixels)
+    }
+    return image
+};
+
+function paintPixel(image: number[][], r: number, c: number, pixelColour, newColour, visitedPixels){
+    const pixel = Number(`${r}${c}`)
+    if(image[r] === undefined || image[r][c] === undefined || image[r][c] !== pixelColour) return
+    image[r][c] = newColour
+    visitedPixels.push(pixel)
+
+    paintPixel(image, r + 1, c, pixelColour, newColour, visitedPixels)
+    paintPixel(image, r - 1, c, pixelColour, newColour, visitedPixels)
+    paintPixel(image, r, c + 1, pixelColour, newColour, visitedPixels)
+    paintPixel(image, r, c - 1, pixelColour, newColour, visitedPixels)
+}
+
+console.log(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2))
