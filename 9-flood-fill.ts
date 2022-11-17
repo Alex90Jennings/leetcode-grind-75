634124d5115ae432d9e982090b95417ad8595e08
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-11-17 10:12:14 +0100

    day 3 from unofficial channel

---
+++ 9-flood-fill.ts
@@ -0,0 +1,5 @@
+function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
+
+};
+
+console.log(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2), " should equal [[2,2,2],[2,2,0],[2,0,1]]")
