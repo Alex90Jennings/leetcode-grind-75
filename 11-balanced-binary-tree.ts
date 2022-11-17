d30e98e8b576a00c73eef88902b9840fb22e61c5
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-11-17 21:34:44 +0100

    day 11 doesn't run with same error as day 10

--- 11-balanced-binary-tree.ts
+++ 11-balanced-binary-tree.ts
@@ -5,11 +5,37 @@ class TreeNodeY {
     constructor(val?: number, left?: TreeNodeY | null, right?: TreeNodeY | null) {
         this.val = (val===undefined ? 0 : val)
         this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
-}
+        this.right = (right===undefined ? null : right)
+    }
 }
 
 
 function isBalanced(root: TreeNodeY | null): boolean {
-    return true
+    let heightRight: number = 0
+    let heightLeft: number = 0
+
+    let nodeRight: TreeNodeY = root[2]
+    let nodeLeft: TreeNodeY = root[1]
+
+    function rightSide(node){
+        if(node.right){
+            heightRight++
+            nodeRight = node.right
+            rightSide(nodeRight)
+        } else return
+    }
+
+    function leftSide(node){
+        if(node.left){
+            heightLeft++
+            nodeLeft = node.left
+            leftSide(nodeLeft)
+        } else return
+    }
+
+    rightSide(nodeRight)
+    leftSide(nodeLeft)
+
+    if(heightLeft - 1 === heightRight || heightLeft + 1 === heightRight || heightLeft === heightRight) return true
+    return false
 };
