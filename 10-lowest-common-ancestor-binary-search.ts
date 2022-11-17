a613f2695b701a54680758af5c3ace49c3fe13f1
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-11-17 21:09:33 +0100

    day 10 does not run

--- 10-lowest-common-ancestor-binary-search.ts
+++ 10-lowest-common-ancestor-binary-search.ts
@@ -11,5 +11,22 @@ class TreeNode {
 
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-	return root || null
+    let i: number = 0
+    const searchedNumbers: number[] = []
+
+    function findNode(p: number){
+        if(root[i].left === p || root[i].right === p) {
+            searchedNumbers.push(root[i].val)
+            i = 0
+        } else {
+            i++
+        }
+    }
+
+    if(searchedNumbers.includes(p.val) || searchedNumbers.includes(q.val)){
+        return p 
+    } else {
+        findNode(p.val)
+        findNode(q.val)
+    }
 };
