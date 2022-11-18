b286944e15c493fd276ab95c4b9817bd5b73afd9
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-11-18 10:00:41 +0100

    day 12 foce method

--- 12-linked-list-cycle.ts
+++ 12-linked-list-cycle.ts
@@ -1,5 +1,18 @@
 import { ListNode } from "./classes";
 
 function hasCycle(head: ListNode | null): boolean {
-    return true
+    const previousHeads: number[] = []
+    let i: number = 0
+    
+    function forceMethod() {
+        const value: number = head[i]
+        if(value){
+            if(previousHeads.includes(value)) return true
+            previousHeads.push(value)
+            i++
+            forceMethod()
+        } else return false
+    }
+
+    return forceMethod()
 };
